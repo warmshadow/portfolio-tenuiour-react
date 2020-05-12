c7abbe2d7fe1dda10aa0dca9bb39be2dc554f934
warmshadow <laukyskarolis@gmail.com>
2020-05-12 23:50:41 +0300

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography';
 import './App.css';
+import Navbar from './Navbar';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      '"Hind"',
+      'sans-serif',
+    ].join(','),
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <div className="App">
+        <Typography variant="h1">TENUIOUR</Typography>
+      </div>
+    </ThemeProvider>
   );
 }
 
